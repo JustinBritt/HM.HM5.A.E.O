f5d7371db784fa2f3c60a92dcb44adb9f14d380c
Justin Britt <britt@uwindsor.ca>
2023-06-15 09:59:05 -0400

    Added factory

---
+++ HM.HM5.A.E.O/Factories/Comparers/OrganizationComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM5.A.E.O.Factories.Comparers
+{
+    using HM.HM5.A.E.O.Classes.Comparers;
+    using HM.HM5.A.E.O.Interfaces.Comparers;
+    using HM.HM5.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class OrganizationComparerFactory : IOrganizationComparerFactory
+    {
+        public OrganizationComparerFactory()
+        {
+        }
+
+        public IOrganizationComparer Create()
+        {
+            IOrganizationComparer instance = null;
+
+            try
+            {
+                instance = new OrganizationComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
