2645578c4189d08801f8872b4d9a1db8920b557d
Justin Britt <britt@uwindsor.ca>
2023-06-15 09:54:44 -0400

    Added class

---
+++ HM.HM5.A.E.O/Classes/Comparers/OrganizationComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM5.A.E.O.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM5.A.E.O.Interfaces.Comparers;
+
+    internal sealed class OrganizationComparer : IOrganizationComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public OrganizationComparer()
+        {
+        }
+
+        public int Compare(
+            Organization x,
+            Organization y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
