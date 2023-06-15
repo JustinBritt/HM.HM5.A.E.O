5ff72f674e8f72195b7b025af352694bfb038288
Justin Britt <britt@uwindsor.ca>
2023-06-15 08:28:49 -0400

    Implement IComparable

--- HM.HM5.A.E.O/Interfaces/IndexElements/Id2IndexElement.cs
+++ HM.HM5.A.E.O/Interfaces/IndexElements/Id2IndexElement.cs
@@ -1,8 +1,10 @@
 ﻿namespace HM.HM5.A.E.O.Interfaces.IndexElements
 {
+    using System;
+
     using Hl7.Fhir.Model;
 
-    public interface Id2IndexElement
+    public interface Id2IndexElement : IComparable<Id2IndexElement>
     {
         /// <summary>
         /// Gets the weekday.
