98c2500cf9b300522eb7d37fda030c5bc7d6524b
Justin Britt <britt@uwindsor.ca>
2023-06-15 08:42:33 -0400

    Implement IComparable

--- HM.HM5.A.E.O/Interfaces/IndexElements/IsIndexElement.cs
+++ HM.HM5.A.E.O/Interfaces/IndexElements/IsIndexElement.cs
@@ -1,8 +1,10 @@
 ﻿namespace HM.HM5.A.E.O.Interfaces.IndexElements
 {
+    using System;
+
     using Hl7.Fhir.Model;
 
-    public interface IsIndexElement
+    public interface IsIndexElement : IComparable<IsIndexElement>
     {
         /// <summary>
         /// Gets the surgeon.
