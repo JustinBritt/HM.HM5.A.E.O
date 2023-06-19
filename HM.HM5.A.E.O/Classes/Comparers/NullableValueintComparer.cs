3420f3586a460ff587ac79018900df3def74cae5
Justin Britt <britt@uwindsor.ca>
2023-06-19 19:18:09 -0400

    Added class

---
+++ HM.HM5.A.E.O/Classes/Comparers/NullableValueintComparer.cs
@@ -0,0 +1,25 @@
+﻿namespace HM.HM5.A.E.O.Classes.Comparers
+{
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM5.A.E.O.Interfaces.Comparers;
+
+    internal sealed class NullableValueintComparer : INullableValueintComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public NullableValueintComparer()
+        {
+        }
+
+        public int Compare(
+            INullableValue<int> x,
+            INullableValue<int> y)
+        {
+            return x.Value.Value.CompareTo(
+                y.Value.Value);
+        }
+    }
+}
