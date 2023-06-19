6b1ba7a10ddea3e416e559e25a7f713084bdda8a
Justin Britt <britt@uwindsor.ca>
2023-06-19 18:45:33 -0400

    Added class

---
+++ HM.HM5.A.E.O/Interfaces/Comparers/ILocationComparer.cs
@@ -0,0 +1,10 @@
+﻿namespace HM.HM5.A.E.O.Interfaces.Comparers
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    public interface ILocationComparer : IComparer<Location>
+    {
+    }
+}
