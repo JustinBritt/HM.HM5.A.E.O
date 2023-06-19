912b5f498f2f5a99bfcc4047f48d2ab4d165c8d4
Justin Britt <britt@uwindsor.ca>
2023-06-19 18:47:26 -0400

    Added factory

---
+++ HM.HM5.A.E.O/Factories/Comparers/LocationComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM5.A.E.O.Factories.Comparers
+{
+    using HM.HM5.A.E.O.Classes.Comparers;
+    using HM.HM5.A.E.O.Interfaces.Comparers;
+    using HM.HM5.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class LocationComparerFactory : ILocationComparerFactory
+    {
+        public LocationComparerFactory()
+        {
+        }
+
+        public ILocationComparer Create()
+        {
+            ILocationComparer instance = null;
+
+            try
+            {
+                instance = new LocationComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
