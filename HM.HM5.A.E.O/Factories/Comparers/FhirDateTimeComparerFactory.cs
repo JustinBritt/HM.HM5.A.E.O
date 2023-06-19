b6d5e77db1a0ab19bf90fdc19b5f34d70d60bb55
Justin Britt <britt@uwindsor.ca>
2023-06-19 18:30:22 -0400

    Added factory

---
+++ HM.HM5.A.E.O/Factories/Comparers/FhirDateTimeComparerFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace HM.HM5.A.E.O.Factories.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using HM.HM5.A.E.O.Classes.Comparers;
+    using HM.HM5.A.E.O.Interfaces.Comparers;
+    using HM.HM5.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class FhirDateTimeComparerFactory : IFhirDateTimeComparerFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public FhirDateTimeComparerFactory()
+        {
+        }
+
+        public IFhirDateTimeComparer Create()
+        {
+            IFhirDateTimeComparer instance = null;
+
+            try
+            {
+                instance = new FhirDateTimeComparer();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
