0f77b8b067e535607598f3f0b7eec825e3337d0f
Justin Britt <britt@uwindsor.ca>
2023-06-20 07:50:41 -0400

    Added visitor

---
+++ HM.HM5.A.E.O/Visitors/Contexts/SurgeonScenarioMaximumNumberPatientMeansInnerVisitor.cs
@@ -0,0 +1,61 @@
+﻿namespace HM.HM5.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM5.A.E.O.Interfaces.IndexElements;
+    using HM.HM5.A.E.O.Interfaces.Indices;
+    using HM.HM5.A.E.O.Interfaces.ParameterElements.SurgeonScenarioMaximumNumberPatients;
+    using HM.HM5.A.E.O.InterfacesFactories.ParameterElements.SurgeonScenarioMaximumNumberPatients;
+    using HM.HM5.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class SurgeonScenarioMaximumNumberPatientMeansInnerVisitor<TKey, TValue> : ISurgeonScenarioMaximumNumberPatientMeansInnerVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : INullableValue<decimal>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioMaximumNumberPatientMeansInnerVisitor(
+            IμParameterElementFactory μParameterElementFactory,
+            IsIndexElement sIndexElement,
+            IΛ Λ)
+        {
+            this.μParameterElementFactory = μParameterElementFactory;
+
+            this.sIndexElement = sIndexElement;
+
+            this.Λ = Λ;
+
+            this.RedBlackTree = new RedBlackTree<IΛIndexElement, IμParameterElement>();
+        }
+
+        private IμParameterElementFactory μParameterElementFactory { get; }
+
+        private IsIndexElement sIndexElement { get; }
+
+        private IΛ Λ { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IΛIndexElement, IμParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IΛIndexElement ΛIndexElement = this.Λ.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                ΛIndexElement,
+                this.μParameterElementFactory.Create(
+                    this.sIndexElement,
+                    ΛIndexElement,
+                    obj.Value));
+        }
+    }
+}
