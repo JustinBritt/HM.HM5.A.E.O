77849dddea8afb56aa5feda35e68f3203cb29de7
Justin Britt <britt@uwindsor.ca>
2023-07-06 20:01:14 -0400

    Added factory

---
+++ HM.HM5.A.E.O/Factories/Dependencies/NGenerics/DataStructures/Trees/RedBlackTreeFactory.cs
@@ -0,0 +1,58 @@
+﻿namespace HM.HM5.A.E.O.Factories.Dependencies.NGenerics.DataStructures.Trees
+{
+    using System;
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using global::NGenerics.DataStructures.Trees;
+
+    using HM.HM5.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+
+    internal sealed class RedBlackTreeFactory : IRedBlackTreeFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public RedBlackTreeFactory()
+        {
+        }
+
+        public RedBlackTree<TKey, TValue> Create<TKey, TValue>()
+        {
+            RedBlackTree<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new RedBlackTree<TKey, TValue>();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+
+        public RedBlackTree<TKey, TValue> Create<TKey, TValue>(
+            IComparer<TKey> comparer)
+        {
+            RedBlackTree<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new RedBlackTree<TKey, TValue>(
+                    comparer);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM5.A.E.O/InterfacesFactories/Dependencies/NGenerics/DataStructures/Trees/IRedBlackTreeFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace HM.HM5.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees
+{
+    using System.Collections.Generic;
+
+    using global::NGenerics.DataStructures.Trees;
+
+    public interface IRedBlackTreeFactory
+    {
+        RedBlackTree<TKey, TValue> Create<TKey, TValue>();
+
+        RedBlackTree<TKey, TValue> Create<TKey, TValue>(
+            IComparer<TKey> comparer);
+    }
+}
