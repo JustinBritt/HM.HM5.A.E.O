61d79d9c382e2081ca894c12e3fd7328585b5d36
Justin Britt <britt@uwindsor.ca>
2023-08-15 11:34:20 -0400

    Added class

---
+++ HM.HM5.A.E.O/Classes/ResultElements/OperatingRoomDayDaySwapAssignments/αResultElement.cs
@@ -0,0 +1,35 @@
+﻿namespace HM.HM5.A.E.O.Classes.ResultElements.OperatingRoomDayDaySwapAssignments
+{
+    using log4net;
+
+    using HM.HM5.A.E.O.Interfaces.IndexElements;
+    using HM.HM5.A.E.O.Interfaces.ResultElements.OperatingRoomDayDaySwapAssignments;
+
+    internal sealed class αResultElement : IαResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public αResultElement(
+            IrIndexElement rIndexElement,
+            Id1IndexElement d1IndexElement,
+            Id2IndexElement d2IndexElement,
+            bool value)
+        {
+            this.rIndexElement = rIndexElement;
+
+            this.d1IndexElement = d1IndexElement;
+
+            this.d2IndexElement = d2IndexElement;
+
+            this.Value = value;
+        }
+
+        public IrIndexElement rIndexElement { get; }
+
+        public Id1IndexElement d1IndexElement { get; }
+
+        public Id2IndexElement d2IndexElement { get; }
+
+        public bool Value { get; }
+    }
+}
---
+++ HM.HM5.A.E.O/Interfaces/ResultElements/OperatingRoomDayDaySwapAssignments/IαResultElement.cs
@@ -0,0 +1,15 @@
+﻿namespace HM.HM5.A.E.O.Interfaces.ResultElements.OperatingRoomDayDaySwapAssignments
+{
+    using HM.HM5.A.E.O.Interfaces.IndexElements;
+
+    public interface IαResultElement
+    {
+        IrIndexElement rIndexElement { get; }
+
+        Id1IndexElement d1IndexElement { get; }
+
+        Id2IndexElement d2IndexElement { get; }
+
+        bool Value { get; }
+    }
+}
