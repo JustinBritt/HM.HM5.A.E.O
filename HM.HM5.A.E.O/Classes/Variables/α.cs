edef5fa2f2f405692ff374117a66ec880d1003ba
Justin Britt <britt@uwindsor.ca>
2023-08-15 12:46:26 -0400

    Added method

--- HM.HM5.A.E.O/Classes/Variables/α.cs
+++ HM.HM5.A.E.O/Classes/Variables/α.cs
@@ -2,10 +2,16 @@
 {
     using log4net;
 
+    using NGenerics.DataStructures.Trees;
+
     using OPTANO.Modeling.Optimization;
 
     using HM.HM5.A.E.O.Interfaces.IndexElements;
+    using HM.HM5.A.E.O.Interfaces.Indices;
+    using HM.HM5.A.E.O.Interfaces.ResultElements.OperatingRoomDayDaySwapAssignments;
     using HM.HM5.A.E.O.Interfaces.Variables;
+    using HM.HM5.A.E.O.InterfacesFactories.ResultElements.OperatingRoomDayDaySwapAssignments;
+    using HM.HM5.A.E.O.InterfacesFactories.Results.OperatingRoomDayDaySwapAssignments;
 
     internal sealed class α : Iα
     {
@@ -32,6 +38,51 @@
             }
 
             return value;
+        }
+
+        public Interfaces.Results.OperatingRoomDayDaySwapAssignments.Iα GetElementsAt(
+            IαResultElementFactory αResultElementFactory,
+            IαFactory αFactory,
+            Id1 d1,
+            Id2 d2,
+            Ir r)
+        {
+            RedBlackTree<IrIndexElement, RedBlackTree<Id1IndexElement, RedBlackTree<Id2IndexElement, IαResultElement>>> outerRedBlackTree = new();
+
+            foreach (IrIndexElement rIndexElement in r.Value.Values)
+            {
+                RedBlackTree<Id1IndexElement, RedBlackTree<Id2IndexElement, IαResultElement>> firstInnerRedBlackTree = new();
+
+                foreach (Id1IndexElement d1IndexElement in d1.Value.Values)
+                {
+                    RedBlackTree<Id2IndexElement, IαResultElement> secondInnerRedBlackTree = new();
+
+                    foreach (Id2IndexElement d2IndexElement in d2.Value.Values)
+                    {
+                        secondInnerRedBlackTree.Add(
+                            d2IndexElement,
+                            αResultElementFactory.Create(
+                                rIndexElement,
+                                d1IndexElement,
+                                d2IndexElement,
+                                this.GetElementAt(
+                                    rIndexElement,
+                                    d1IndexElement,
+                                    d2IndexElement)));
+                    }
+
+                    firstInnerRedBlackTree.Add(
+                        d1IndexElement,
+                        secondInnerRedBlackTree);
+                }
+
+                outerRedBlackTree.Add(
+                    rIndexElement,
+                    firstInnerRedBlackTree);
+            }
+
+            return αFactory.Create(
+                outerRedBlackTree);
         }
     }
 }
--- HM.HM5.A.E.O/Interfaces/Variables/Iα.cs
+++ HM.HM5.A.E.O/Interfaces/Variables/Iα.cs
@@ -3,6 +3,9 @@
     using OPTANO.Modeling.Optimization;
 
     using HM.HM5.A.E.O.Interfaces.IndexElements;
+    using HM.HM5.A.E.O.Interfaces.Indices;
+    using HM.HM5.A.E.O.InterfacesFactories.ResultElements.OperatingRoomDayDaySwapAssignments;
+    using HM.HM5.A.E.O.InterfacesFactories.Results.OperatingRoomDayDaySwapAssignments;
 
     public interface Iα
     {
@@ -12,5 +15,12 @@
             IrIndexElement rIndexElement,
             Id1IndexElement d1IndexElement,
             Id2IndexElement d2IndexElement);
+
+        Interfaces.Results.OperatingRoomDayDaySwapAssignments.Iα GetElementsAt(
+            IαResultElementFactory αResultElementFactory,
+            IαFactory αFactory,
+            Id1 d1,
+            Id2 d2,
+            Ir r);
     }
 }
