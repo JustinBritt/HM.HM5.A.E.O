d1b83702f5e7bcfa9eb80adaa09a790bb4fdf7da
Justin Britt <britt@uwindsor.ca>
2023-08-15 11:57:53 -0400

    Added factory

---
+++ HM.HM5.A.E.O/Factories/Results/OperatingRoomDayDaySwapAssignments/αFactory.cs
@@ -0,0 +1,43 @@
+﻿namespace HM.HM5.A.E.O.Factories.Results.OperatingRoomDayDaySwapAssignments
+{
+    using System;
+
+    using log4net;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM5.A.E.O.Classes.Results.OperatingRoomDayDaySwapAssignments;
+    using HM.HM5.A.E.O.Interfaces.IndexElements;
+    using HM.HM5.A.E.O.Interfaces.ResultElements.OperatingRoomDayDaySwapAssignments;
+    using HM.HM5.A.E.O.Interfaces.Results.OperatingRoomDayDaySwapAssignments;
+    using HM.HM5.A.E.O.InterfacesFactories.Results.OperatingRoomDayDaySwapAssignments;
+
+    internal sealed class αFactory : IαFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public αFactory()
+        {
+        }
+
+        public Iα Create(
+            RedBlackTree<IrIndexElement, RedBlackTree<Id1IndexElement, RedBlackTree<Id2IndexElement, IαResultElement>>> value)
+        {
+            Iα instance = null;
+
+            try
+            {
+                instance = new α(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM5.A.E.O/InterfacesFactories/Results/OperatingRoomDayDaySwapAssignments/IαFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace HM.HM5.A.E.O.InterfacesFactories.Results.OperatingRoomDayDaySwapAssignments
+{
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM5.A.E.O.Interfaces.IndexElements;
+    using HM.HM5.A.E.O.Interfaces.ResultElements.OperatingRoomDayDaySwapAssignments;
+    using HM.HM5.A.E.O.Interfaces.Results.OperatingRoomDayDaySwapAssignments;
+
+    public interface IαFactory
+    {
+        Iα Create(
+            RedBlackTree<IrIndexElement, RedBlackTree<Id1IndexElement, RedBlackTree<Id2IndexElement, IαResultElement>>> value);
+    }
+}
